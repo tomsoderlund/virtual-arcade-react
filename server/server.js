fff2148122c2ce2e2eb657c6ea67ae635d058993
Tom Söderlund <tom@tomsoderlund.com>
2019-04-20 00:47:56 +0200

    Serve any route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.get('/ping', function (req, res) {
   return res.send('pong')
 })
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, '../build/index.html'))
 })
 
